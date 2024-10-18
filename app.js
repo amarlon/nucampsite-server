19f0092edf87ad8299c0398ee1b1b317f89241d5
amarlon <marlonluzayamo@gmail.com>
2024-10-18 14:01:25 -0400

    Express Session Part 2

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
+const mongoose = require("mongoose");
+const url = 'mongodb://localhost:27017/nucampsite';
+
+const connect = mongoose.connect(url, {});
+
+connect.then(() => console.log('connected correctly to server'),
+    err => console.log(err)
+);
 
 var app = express();
 
@@ -16,26 +30,59 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser('12344-343234-0948485-543948'));
+app.use(session({
+    name: 'session-id',
+    secret: '12344-343234-0948485-543948',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+function auth(req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!sa');
+        err.status = 401;
+        return next(err);
+    } else {
+        if (req.session.user === 'authenticated') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticatedsss!');
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
